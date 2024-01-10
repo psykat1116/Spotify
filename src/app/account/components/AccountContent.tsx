57fcf2c75fa6488e3be046dbfbf82797886d68e8
Saikat Samanta <95569573+psykat1116@users.noreply.github.com>
2024-01-10 13:52:17 +0530

    Bug Fixed in useRouter

--- src/app/account/components/AccountContent.tsx
+++ src/app/account/components/AccountContent.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React, { use, useEffect, useState } from "react";
 import useSubscriptionModal from "../../../../hooks/useSubscriptionModal";
 import { useUser } from "../../../../hooks/useUser";
